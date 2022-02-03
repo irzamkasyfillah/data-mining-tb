b5a3f46c738d2431f6d0e7e6961b5ae2afd22113
Alfarabi Alif <alfarabiabhy123@gmail.com>
2022-02-03 11:07:38 +0800

    get datetime and change into string

--- api/main.py
+++ api/main.py
@@ -7,6 +7,7 @@ from datetime import date
 
 app = FastAPI()
 dataset = 'api/csv/databaru.csv'
+start = date.today()
 format = "%Y%m%d_%H%M%S"
 
 
@@ -17,7 +18,6 @@ async def root(start_date: date = start):
 
 @app.post("/upload-file/")
 async def create_upload_file(uploaded_file: UploadFile = File(...)):
-    start = date.today()
     date_time = start.strftime(format)
     file_location = f"files/{ date_time + uploaded_file.filename}"
     with open(file_location, "wb+") as file_object:
