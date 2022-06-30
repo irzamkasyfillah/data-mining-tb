b4822dbf653f9db86e13ffce402f29702d4f68f8
Alfarabi Alif <alfarabiabhy123@gmail.com>
2022-06-30 20:14:23 +0800

    table asosiasi

--- pages/asosiasi/index.js
+++ pages/asosiasi/index.js
@@ -205,19 +205,9 @@ function Asosiasi() {
         <div
           className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
         >
-          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="relative w-auto h-5/6 my-6 mx-auto max-w-3xl">
             {/*content*/}
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              {/*header*/}
-              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  </span>
-                </button>
-              </div>
               {/*body*/}
               <div className="relative p-6 flex-auto">
                 <AturanAsosiasi modalData={modalData}/>
@@ -230,13 +220,6 @@ function Asosiasi() {
                   onClick={() => setShowModal(false)}
                 >
                   Close
-                </button>
-                <button
-                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Save Changes
                 </button>
               </div>
             </div>
