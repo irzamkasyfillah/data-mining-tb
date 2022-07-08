a20f52f15167bcc1fc6206e4febe079dbad8acca
Alfarabi Alif <alfarabiabhy123@gmail.com>
2022-07-09 07:45:28 +0800

    Update AturanAsosiasi.js

--- src/components/AturanAsosiasi.js
+++ src/components/AturanAsosiasi.js
@@ -10,6 +10,7 @@ export function AturanAsosiasi({modalData}) {
             {
                 name: 'Aturan Asosiasi',
                 selector: (row) => row.aturan,
+                width: "480px",
             },
         ]
 
